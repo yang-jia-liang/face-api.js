29a3eb200d7178ad43ae14d32450b16a5c31ec5e
yang-jia-liang <528748995@qq.com>
2022-07-31 13:08:37 +0800

    新增自动发布

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+# 生成静态文件
+yarn run build
+
+# 进入生成的文件夹
+cd dist
+
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:<BranchName>
+git push -f git@github.com:yang-jia-liang/face-api.js.git master:gh-pages
+
+cd -
+
+# 发布到github page： 项目根目录 Git Bash Here -> yarn run deploy-gh
+
--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
 
+  publicPath: './',
+
   //关闭eslint校验
   lintOnSave: false,
 })
